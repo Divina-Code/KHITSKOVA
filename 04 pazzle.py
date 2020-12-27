6e68ae6ff6219a7bfc323e0ec10c84ce1f0830af
dariakhitskova <76618202+dariakhitskova@users.noreply.github.com>
2020-12-27 19:07:13 +0300

    Update 04 pazzle.py

--- 04 pazzle.py
+++ 04 pazzle.py
@@ -1,14 +1,14 @@
 
-isGuessed=7
+isGuessed=False
 
-while isGuessed!=7:
+while isGuessed!=True:
     puzzel="В вазе было 12 конфет. 5 конфет съели. Сколько конфет осталось?"
 
     answer=input(puzzle)
 
     if answer==7:
         print("Дааа,ты молодец.")
-        isGuessed=7
+        isGuessed=True
     if answer!=7:
         print("неправильно, попробуй снова")
 
